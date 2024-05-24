8206652f7f22aedb50852fd7d51db69dbf7f93a6
rootKid <137388933+devMainak@users.noreply.github.com>
2024-05-24 07:56:11 +0000

    seeding data function added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { initializeDatabase } = require('./db/db.connected')
 const fs = require('fs')
+const Books = require('./models/books.model') 
 
 // Establishing connection to DB
 initializeDatabase();
@@ -8,3 +9,30 @@ initializeDatabase();
 const jsonData = fs.readFileSync('books.json')
 // Parsing the raw json data
 const booksData = JSON.parse(jsonData)
+
+// function to seed intial data to the schema
+const seedData = () => {
+  try {
+    for (const book of booksData)
+      {
+        const newBook = new Books({
+          title: book.title,
+          author: book.author,
+          publishedYear: book.publishedYear,
+          genre: book.genre,
+          language: book.language,
+          country: book.country,
+          rating: book.rating,
+          summary: book.summary,
+          coverImageUrl: book.coverImageUrl
+        })
+
+        newBook.save()
+        console.log("New Book: ", newBook.title)
+      }
+  } catch(error) {
+    console.error("Error occured while seeding data to DB")
+  }
+}
+
+seedData()
