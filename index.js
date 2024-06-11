5ee5adca49ca0b8a2889a5455588b63c3c8b6565
rootKid <137388933+devMainak@users.noreply.github.com>
2024-06-11 05:45:56 +0000

    feat: GET-/books

--- index.js
+++ index.js
@@ -37,7 +37,45 @@ app.post("/books", async (req, res) => {
   }
 })
 
+// function to read all book from db
+const readAllBooks = async () => {
+  try {
+    const books = await Books.find()
+    return books
+  } catch(error) {
+    throw error
+  }
+}
 
+// GET method on "/books" to read all books
+app.get("/books", async (req, res) => {
+  try {
+   const books = await readAllBooks()
+    if (books.length != 0) {
+      res.status(201)
+      .json(books)
+    } else {
+      res.status(404)
+      .json({error: "No books found"})
+    }
+  } catch(error) {
+    res.status(500)
+    .json({error: "Internal server error"})
+    console.error(error)
+  }
+})
+
+// function to read book data by title
+const readBookByTitle = async (bookTitle) => {
+  try {
+    const book = await Books.findOne({title: bookTitle})
+    return book
+  } catch(error) {
+    throw error
+  }
+}
+
+// GET method 
 
 // Listenting to http port for http requests
 const PORT = 3000
