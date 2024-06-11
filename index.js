19365fd3c04b2ae181f798ff9830163dd7b7463b
rootKid <137388933+devMainak@users.noreply.github.com>
2024-06-11 05:27:35 +0000

    feat: POST-/books

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
+const express = require('express')
+const app = express()
 const { initializeDatabase } = require('./db/db.connected')
-const fs = require('fs')
 const Books = require('./models/books.model') 
+
+app.use(express.json())
 
 // Establishing connection to DB
 initializeDatabase();
 
-// Reading the raw json data
-const jsonData = fs.readFileSync('books.json')
-// Parsing the raw json data
-const booksData = JSON.parse(jsonData)
-
-// function to seed intial data to the schema
-const seedData = () => {
+// function to seed intial book to the schema
+const seedBook = async (bookData) => {
   try {
-    for (const book of booksData)
-      {
-        const newBook = new Books({
-          title: book.title,
-          author: book.author,
-          publishedYear: book.publishedYear,
-          genre: book.genre,
-          language: book.language,
-          country: book.country,
-          rating: book.rating,
-          summary: book.summary,
-          coverImageUrl: book.coverImageUrl
-        })
-
-        newBook.save()
-        console.log("New Book: ", newBook.title)
-      }
+    const book = new Books(bookData)
+    const savedBook = await book.save()
+    return savedBook
   } catch(error) {
-    console.error("Error occured while seeding data to DB")
+    throw error
   }
 }
 
-seedData()
+// POST route on "/books" path to save books in DB
+app.post("/books", async (req, res) => {
+  try {
+    const savedBook = await seedBook(req.body)
+    if (savedBook) {
+      res.status(201)
+      .json({message: "Book added successfully", savedBook: savedBook})
+    } else {
+      res.status(400)
+      .json({error: "Failed to add book"})
+    }
+  } catch(error) {
+    res.status(500)
+    .json({error: "Internal server error"})
+    console.error(error)
+  }
+})
+
+
+
+// Listenting to http port for http requests
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
